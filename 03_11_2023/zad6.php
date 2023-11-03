f2d8612d46948fa963a80485338d641a5676169b
Chimera11 <damian.manecki@gmail.com>
2023-11-03 12:01:26 +0100

    Add files via upload

--- 03_11_2023/zad6.php
+++ 03_11_2023/zad6.php
@@ -1,12 +1,16 @@
 <?php
-    $sideA = 3;
+    $sideA = 4;
     $sideB = 4;
-    $sideC = 5;
+    $sideC = 2;
     if(is_numeric($sideA) && is_numeric($sideB)  && is_numeric($sideC)) 
     {
         if($sideA == $sideB && $sideB == $sideC)
         {
             echo"trójkąt jest równoboczny";
+        }
+        else if(($sideA*$sideA+$sideB*$sideB==$sideC*$sideC) || ($sideA*$sideA+$sideC*$sideC==$sideB*$sideB) || ($sideC*$sideC+$sideB*$sideB==$sideA*$sideA) && $sideA == $sideB && $sideB == $sideC)
+        {
+            echo"trójkąt równoramienny i prostokątny";
         }
         else if($sideA == $sideB || $sideB == $sideC || $sideA == $sideC)
         {
@@ -20,6 +24,7 @@
         {
         echo"Trojkąt prostokątny";
         }
+        
         else
         {
             echo"inny";
