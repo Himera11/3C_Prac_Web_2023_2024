8a07ddf7e17e3d2795c91180e3cad4c94bfc87d2
Chimera11 <damian.manecki@gmail.com>
2024-02-09 12:07:24 +0100

    Add files via upload

--- 02.02.2024/zad egzaminacyjne 1.php
+++ 02.02.2024/zad egzaminacyjne 1.php
@@ -2,8 +2,7 @@
     echo "zarestrowano nowego użytkownika. \n Dane:<br>";
     echo "rok urodzenia: ".$_POST["data"]."<br>";
     echo "miejsce urodzenia: ".$_POST["miejsce"]."<br>";
-    echo "płeć: ".$_POST["płeć"]."<br>";
+    echo "płeć: " .$_POST['płeć']."<br>";
     echo "wyrazenie zgody: ".$_POST["zgoda"]."<br>";
-
 ?>
 
